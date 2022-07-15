705eedc60a52c205aac65988916b52322b28a8a4
Cake <cakemancerr@gmail.com>
2022-07-15 23:35:43 +0200

    Refactored colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"rose": {
-					"50": "#FFFAFE",
-					"100": "#FFF5FC",
-					"200": "#FEE6F8",
-					"300": "#FEDCF5",
-					"400": "#FDCEF1",
-					"500": "#FDC1ED",
-					"600": "#FA6BD4",
-					"700": "#F817BC",
-					"800": "#AD0680",
-					"900": "#590342"
-				}
-			}
+				'light': '#f3ede8',
+				'dark': '#2a0416',
+				'bgc': '#fdc8bc',
+				'accent': '#e6516d',
+
+			},
 		},
 	},
 	plugins: [],
