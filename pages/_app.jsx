7520e1776f0f1a3d2794ccd7e2897f12bd84f0bf
Cake <cakemancerr@gmail.com>
2022-09-02 23:22:08 +0200

    Add site title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 import '../styles/global.css';
 import Nav from '../components/layout/nav';
 import { CartProvider } from '../context/cartContext';
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<CartProvider>
+			<Head>
+				<title>Cucucakes</title>
+			</Head>
 			<Nav />
 			<Component {...pageProps} />
 		</CartProvider>
